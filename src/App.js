b10210340065222bb265699201b7cfb36e04e68f
amirbaum <colocba@gmail.com>
2019-07-17 20:58:53 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://raw.githubusercontent.com/colocba/elk/master/public/assets/logo1.png" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Sorry but this domain is not working. We are available here:
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://elk-moving-hauling.onrender.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          https://elk-moving-hauling.onrender.com/
         </a>
       </header>
     </div>
